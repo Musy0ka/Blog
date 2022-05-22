29701fb2c5ac1aba1701e55a1ee05f7160cb92f1
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-23 02:17:10 +0300

    added init logic

--- website/__init__.py
+++ website/__init__.py
@@ -5,6 +5,14 @@ from flask_login import login_manager
 
 def create_app():
   app = Flask (__name__)
-  app.config('SECRET_KEY')-'secretkey'
+  app.config['SECRET_KEY'] = 'secretkey'
+
+  @app.route("/")
+  def home():
+    return "<h1>Hello</h1>"
+
+  @app.route("/profile")
+  def profile():
+    return "<h1>Profile</h1>"
 
   return app
