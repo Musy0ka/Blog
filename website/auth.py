aa7019ed48f516e2c75ec2ac2245bab8d0deae1c
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-23 06:47:30 +0300

    added post database model

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,8 @@
-from crypt import methods
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-
 
 auth = Blueprint("auth", __name__)
 
@@ -25,7 +23,8 @@ def login():
                 flash('Password is incorrect.', category='error')
         else:
             flash('Email does not exist.', category='error')
-    return render_template("login.html")
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
@@ -59,7 +58,8 @@ def sign_up():
             login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for('views.home'))
-    return render_template("signup.html")
+
+    return render_template("signup.html", user=current_user)
 
 
 @auth.route("/logout")
